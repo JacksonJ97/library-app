a6b182fd4ade407fcac01b9fbeac8cbeed9d4b3c
JacksonJ97 <jacksonjude97@gmail.com>
2021-08-06 04:35:19 -0400

    cleaned up the JS code

--- script.js
+++ script.js
@@ -28,37 +28,32 @@ function updateLocalStorage() {
   localStorage.setItem("library", JSON.stringify(myLibrary));
 }
 
-if (localStorage.getItem("library") === null) {
-  myLibrary = [];
-} else {
-  let savedLibrary = JSON.parse(localStorage.getItem("library"));
-
-  for (let i = 0; i < savedLibrary.length; i++) {
-    const book = new Book(savedLibrary[i].title, savedLibrary[i].author, savedLibrary[i].pages, savedLibrary[i].isRead);
-    myLibrary.push(book);
-  }
-
-  displayBooks(myLibrary);
-}
-
-function addBookToLibrary() {
-  const book = new Book(titleInputElement.value, authorInputElement.value, pagesInputElement.value, readInputElement.checked);
-  myLibrary.push(book);
-  updateLocalStorage();
-}
-
-function displayAddedBook(libraryArr) {
-  const bookCard = createBookCardElement(libraryArr[libraryArr.length - 1]);
-  bookCard.setAttribute("data-index", libraryArr.length - 1);
-  booksContainer.append(bookCard);
-}
-
 function displayBooks(libraryArr) {
   for (let i = 0; i < libraryArr.length; i++) {
     const bookCard = createBookCardElement(libraryArr[i]);
     bookCard.setAttribute("data-index", i);
     booksContainer.append(bookCard);
   }
+}
+
+if (localStorage.getItem("library") === null) {
+  myLibrary = [];
+} else {
+  let savedLibrary = JSON.parse(localStorage.getItem("library"));
+
+  // Using the local storage data to recreate the book object which includes the Book.prototype
+  for (let i = 0; i < savedLibrary.length; i++) {
+    const book = new Book(savedLibrary[i].title, savedLibrary[i].author, savedLibrary[i].pages, savedLibrary[i].isRead);
+    myLibrary.push(book);
+  }
+
+  displayBooks(myLibrary); // To display the books saved in local storage when the page refreshes
+}
+
+function addBookToLibrary() {
+  const book = new Book(titleInputElement.value, authorInputElement.value, pagesInputElement.value, readInputElement.checked);
+  myLibrary.push(book);
+  updateLocalStorage();
 }
 
 function createBookCardElement(book) {
@@ -69,12 +64,14 @@ function createBookCardElement(book) {
   const bookReadStatusBtn = document.createElement("button");
   const removeBtn = document.createElement("button");
 
-  bookReadStatusBtn.classList.add("book-read-status-btn");
   bookCardDiv.classList.add("book-card");
+  bookReadStatusBtn.classList.add("book-read-status-btn");
+  removeBtn.classList.add("remove-btn");
 
   bookTitle.textContent = book.title;
   bookAuthor.textContent = book.author;
   bookPages.textContent = book.pages;
+  removeBtn.textContent = "Remove";
 
   if (book.isRead) {
     bookReadStatusBtn.textContent = "Read";
@@ -83,9 +80,6 @@ function createBookCardElement(book) {
     bookReadStatusBtn.textContent = "Not Read";
     bookReadStatusBtn.style.backgroundColor = "rgb(248, 125, 116)";
   }
-
-  removeBtn.textContent = "Remove";
-  removeBtn.classList.add("remove-btn");
 
   bookCardDiv.append(bookTitle);
   bookCardDiv.append(bookAuthor);
@@ -111,6 +105,8 @@ function createBookCardElement(book) {
     const index = bookCardDiv.getAttribute("data-index");
     myLibrary.splice(index, 1);
     booksContainer.removeChild(bookCardDiv);
+
+    // Re-assigning the data-index values
     const booksContainerChildrenArr = Array.from(booksContainer.childNodes);
 
     for (let i = 0; i < booksContainerChildrenArr.length; i++) {
@@ -123,12 +119,29 @@ function createBookCardElement(book) {
   return bookCardDiv;
 }
 
+function displayAddedBook(libraryArr) {
+  const bookCard = createBookCardElement(libraryArr[libraryArr.length - 1]);
+  booksContainer.append(bookCard);
+
+  // Setting the data-index attribute to the corresponding book card
+  bookCard.setAttribute("data-index", libraryArr.length - 1);
+}
+
 function resetForm() {
   titleInputElement.value = "";
   authorInputElement.value = "";
   pagesInputElement.value = "";
   readInputElement.checked = false;
 }
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  addBookToLibrary();
+  displayAddedBook(myLibrary);
+  resetForm();
+});
+
+// Event Listeners for the modal //
 
 newBookBtn.addEventListener("click", function () {
   modal.style.display = "block";
@@ -140,11 +153,4 @@ window.addEventListener("click", function (e) {
   } else if (e.target == form.lastElementChild) {
     modal.style.display = "none";
   }
-});
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  addBookToLibrary();
-  resetForm();
-  displayAddedBook(myLibrary);
 });
